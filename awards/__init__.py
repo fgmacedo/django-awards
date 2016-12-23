2ee5afbf2eacf61467fa7b7c1fed64541078caa4
Fernando Macedo <fgmacedo@gmail.com>
2016-12-23 11:33:23 -0200

    Bump version: 0.1.0 → 0.2.0

--- awards/__init__.py
+++ awards/__init__.py
@@ -1,7 +1,7 @@
 from .internals import badges
 from .base import Badge, BadgeDetail, BadgeAwarded  # NOQA
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 default_app_config = 'awards.apps.AwardsConfig'
 
